fd7577970b5dc9fa119fea7dee1598bcc881e4f4
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-11-23 22:23:05 +0530

    initial commit

---
+++ src/main/java/com/spring/springbootproductcatlog/SpringBootProductCatlogApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springbootproductcatlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductCatlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProductCatlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springbootproductcatlog/SpringBootProductCatlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootproductcatlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductCatlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
